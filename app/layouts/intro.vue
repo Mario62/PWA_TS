0ef5204875128003d0525898d6c39b31ae078977
Mariusz Pierzchała <mario.pierzchala@outlook.com>
2020-06-22 20:30:28 +0200

    Navbar

--- app/layouts/intro.vue
+++ app/layouts/intro.vue
@@ -1,35 +1,45 @@
 <template>
   <div class="box">
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand ><NuxtLink to="/" class="homelink">M-Book</NuxtLink></b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item href="https://bootstrap-vue.org/docs/components/badge">Components</b-nav-item>
-          <b-nav-item href="https://github.com/Mario62/PWA_TS/tree/dev">
-            <b-badge variant="light">Alpha</b-badge>
-          </b-nav-item>
-        </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-            
-          </b-nav-form>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+    <Navbar />
     <div>
       <nuxt />
     </div>
   </div>
 </template>
+<script lang="ts">
+// import { store } from '~/store/index.js'
 
+import Vue from 'vue'
+import Navbar from '~/components/Navbar.vue'
+
+export default Vue.extend({
+  components: {
+    Navbar
+  },
+
+  methods: {
+    // warn: function(message, event) {
+    //   // now we have access to the native event
+    //   if (event) {
+    //     event.preventDefault()
+    //   }
+    //   this.$store.commit('increment')
+    //   // this.$store
+    //   console.log(this.$store.state.count) // -> 1
+    //   // this.$store.dispatch('loadData')
+    // alert(Regex.getRandom)
+    //   // alert(message)
+    // },
+    
+  },
+  mounted() {
+    
+  }
+})
+</script>
 <style>
+
+
 .box {
   background-color:antiquewhite;
 }
