d464035ad90c518a0181933868da9bda78e964cf
informacja <piotrwpwsz@gmail.com>
2020-06-22 21:27:06 +0200

    w

--- app/layouts/intro.vue
+++ app/layouts/intro.vue
@@ -44,9 +44,9 @@ export default Vue.extend({
   background-color:antiquewhite;
   margin: 0 auto;
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  /* display: flex; */
+  /* justify-content: center; */
+  /* align-items: center; */
   text-align: center;
 }
 html {
