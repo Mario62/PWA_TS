0aa9c941fe4c7c1b8ee5f4277e652921febb4b54
informacja <piotrwpwsz@gmail.com>
2020-06-29 11:42:13 +0200

    to pw

--- app/store/index.js
+++ app/store/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 export const state = () => ({
   announcement: null
 })
@@ -7,12 +9,22 @@ export const mutations = {
     state.announcement = message
   }
 }
-
 export const actions = {
   async nuxtServerInit({ commit }) {
-    const { body } = await fetch(
-      'https://jsonplaceholder.typicode.com/posts/1'
-    ).then((response) => response.json())
+    const { body } = await axios
+      .get('api/v1/2.20.min.json')
+      // .get('api/v1/06.2020.json')
+      .then((response) => response.json())
+    // .catch(error) { console.warn(error)
+    // }
+
     commit('SET_ANNOUNCEMENT', body)
+
+    //// Working example
+    // const { body } = await fetch(
+    //   'https://jsonplaceholder.typicode.com/posts/1'
+    // ).then((response) => response.json())
+    // console.warn(body)
+    // commit('SET_ANNOUNCEMENT', body)
   }
 }
